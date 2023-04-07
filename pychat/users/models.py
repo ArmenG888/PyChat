d7eb837853d3a943f9c46b39a1cbc2fecd58ed57
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-04-06 17:34:15 -0700

    added profile models

--- pychat/users/models.py
+++ pychat/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.conf import settings
+from django.utils import timezone
+import datetime
+User = settings.AUTH_USER_MODEL
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    last_online = models.DateTimeField(default=timezone.now)
+    def online(self):
+        widthin_last_seconds = timezone.now() - datetime.timedelta(minutes=5)
+        if self.last_online > widthin_last_seconds:
+            return "🟢"
+        else:
+            return "🔴"

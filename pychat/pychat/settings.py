a74c1fc9923bdef63b6d646641f3e71d478c64e2
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-04-23 09:29:07 -0700

    bug fix

--- pychat/pychat/settings.py
+++ pychat/pychat/settings.py
@@ -126,7 +126,7 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
+SUCCESS_URL = ""
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
